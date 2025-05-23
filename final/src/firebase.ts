3f86efd8f8a6288acac3ba20889231e52c46fa0a
ZhuoDong Zhen <88686053+d940909492@users.noreply.github.com>
2025-05-22 20:39:07 -0400

    Update firebase.ts

--- final/src/firebase.ts
+++ final/src/firebase.ts
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+        import { initializeApp } from "firebase/app";
+        import { getAuth } from "firebase/auth";
+        import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCSWVbh4vSXeu7pgCKo5Ddqw2ivKE--Kfw",
-  authDomain: "final-proj-ece01.firebaseapp.com",
-  projectId: "final-proj-ece01",
-  storageBucket: "final-proj-ece01.firebasestorage.app",
-  messagingSenderId: "826171583332",
-  appId: "1:826171583332:web:0dada8ea3b1caadf8d6871"
-};
+        const firebaseConfig = {
+          apiKey: "YOUR_API_KEY",
+          authDomain: "YOUR_AUTH_DOMAIN",
+          projectId: "YOUR_PROJECT_ID",
+          storageBucket: "YOUR_STORAGE_BUCKET",
+          messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+          appId: "YOUR_APP_ID"
+        };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+        const app = initializeApp(firebaseConfig);
+        const auth = getAuth(app);
+        const db = getFirestore(app);
 
-export { app, auth, db };
+        export { app, auth, db };
